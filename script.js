3f3a1f039175edd2bcc5fddf5b8673e2fe34da45
nwlilley <nathaniel.lilley@gmail.com>
2020-01-31 17:22:14 -0500

    fix #2 to toggle descriptive text

--- script.js
+++ script.js
@@ -1,77 +1,41 @@
 console.log('Add validation!');
-// function () {
-//     let inputField = document.querySelectorAll(".input-field")
-//     for (let i = 0; i < inputField.length; i++) {}
-//         if (inputField[i].innerText == "") {
-//         inputField[i].classList.add = "input-invalid"
-//     } else {
-//         inputField.className.add = "input-valid"
-//     }
-// }
 
 let inputFields = document.querySelectorAll(".input-field")
 
-let isRequired = " is required"
+let isRequired = " info is required"
 // let fields = document.querySelectorAll(".field")
 
-let el = document.querySelector("form")
-el.addEventListener("submit", function(event) {
+let form = document.querySelector("#parking-form")
+
+.addEventListener("submit", function(event) {
     event.preventDefault();
     for (let field of inputFields) {
+        
         if (field.querySelector("input").value == "") { 
             if (field.classList.contains("input-invalid")) {
                 console.log("do nothing")
+                
             } else {
-                console.log("empty")
+                field.classList.remove("input-valid")
                 field.classList.add("input-invalid")
-                field.querySelector("label").innerHTML += isRequired
+                if (field.querySelector("label").innerHTML.includes(isRequired) == false) {
+                    field.querySelector("label").innerHTML += isRequired
+                }
+                
             }  
         } else { 
             console.log("not empty")
+            field.classList.remove("input-invalid")
             field.classList.add("input-valid")
-            // if (field.classList.contains("input-invalid")) {
-            //     field.classList.remove("input-invalid")
-            //     field.querySelector("label").innerHTML.replace(" is required", " ")
-            // }
+            if (field.querySelector("label").innerHTML.includes(isRequired) == true) {
+                field.querySelector("label").innerHTML = field.querySelector("label").innerHTML.replace(isRequired, "")
+            }
         }
     }
+
+        
+            
+        
+    
 })
-
-// let submitButton = document.querySelector("#submit-button");
-
-// let parkingForm = document.querySelector("#parking-form")
-
-// submitButton.addEventListener("submit", function () {
-//     let inputField = document.querySelectorAll(".input-field")
-//     for (let i = 0; i < inputField.length; i++) {
-//         if (inputField[i].value == "") {
-//         console.log("empty")
-//         inputField[i].classList.add("input-invalid")
-//         } else {
-//         console.log("not empty")
-//         inputField[i].classList.add("input-valid")
-//         }
-//     }
-// })
-
-// let el = document.querySelector("form");
-
-// el.addEventListener("submit", function(e) {
-//     e.preventDefault(); 
-// })
-
-// let nameInput = document.querySelector("#name")
-// let nameValue = nameInput.value
-// let parentDiv = nameInput.parentNode;
-
-// if (nameValue != "") {
-//     console.log("not empty")
-//     parentDiv.classList.remove("input-invalid")
-//     parentDiv.classList.add("input-valid")
-// } else {
-//     console.log("empty")
-//     parentDiv.classList.remove("input-valid")
-//     parentDiv.classList.add("input-invalid")
-// }
-
 
